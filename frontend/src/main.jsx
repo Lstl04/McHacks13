808784f76e666af61cf58dd137393a9494093ed0
Lucas Fernandes <lucasfrd@icloud.com>
2026-01-17 15:39:18 -0500

    updated main + package + config files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react;'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-auxamjb2ab0y6cyh.us.auth0.com"
+      clientId="8yvpeSxHpnqEv2UIBVplGSDqlS7yxo9j"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
